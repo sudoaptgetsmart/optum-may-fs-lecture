ef2d9bf51c345a23a38e8ce60fb25a480abba482
mfortenberry <mfortenberry@yorksolutions.net>
2022-06-20 10:18:14 -0500

    ip.jsontest.com proxy

--- src/main/java/net/yorksolutions/myfirstjavaproject/Controller.java
+++ src/main/java/net/yorksolutions/myfirstjavaproject/Controller.java
@@ -2,7 +2,9 @@ package net.yorksolutions.myfirstjavaproject;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 // Controller - is a collection of methods that respond to http requests
 // RestController - expects the http requests to follow RESTful guidlines
@@ -13,5 +15,23 @@ public class Controller {
     @GetMapping("/hello")
     String helloWorld() {
         return "hello world";
+    }
+
+    @GetMapping("/hi")
+    String hi() {
+        return "hi";
+    }
+
+    // URL parameter http://localhost:8080/param?someArg=val1&arg2=val2
+    @GetMapping("/param")
+    String hi(@RequestParam(name = "someArg") String arg1, String arg2) {
+        return "You passed " + arg1 + " and " + arg2;
+    }
+
+    // Act as a middle man for ip.jsontest.com
+    @GetMapping("/ip")
+    String ip() {
+        final RestTemplate rest = new RestTemplate();
+        return rest.getForObject("http://ip.jsontest.com", String.class);
     }
 }

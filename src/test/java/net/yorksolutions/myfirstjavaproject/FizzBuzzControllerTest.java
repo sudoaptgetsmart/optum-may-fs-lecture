7731dd476e392cf18626570139fa52de70857e52
sudoaptgetsmart <tfrye463@gmail.com>
2022-06-23 10:13:49 -0500

    fixed controller test running

--- src/test/java/net/yorksolutions/myfirstjavaproject/FizzBuzzControllerTest.java
+++ src/test/java/net/yorksolutions/myfirstjavaproject/FizzBuzzControllerTest.java
@@ -1,19 +1,24 @@
 package net.yorksolutions.myfirstjavaproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FizzBuzzControllerTest {
+    @LocalServerPort
+    int port;
+
     @Test
     void fizzbuzz() {
         final RestTemplate rest = new RestTemplate();
-        final ResponseEntity<String> actual = rest.getForEntity("http://localhost:8080/fizzbuzz?input=4", String.class);
+        final ResponseEntity<String> actual = rest.getForEntity("http://localhost:" + port + "/fizzbuzz?input=4", String.class);
         assertEquals(HttpStatus.OK, actual.getStatusCode());
         assertEquals("4", actual.getBody());
     }
